ed6f08a71c8787f401f0ade970e8c171f8808def
gilkarin <gilkarin8@gmail.com>
2022-06-01 21:51:34 +0300

    first

--- task_1_p1.py
+++ task_1_p1.py
@@ -1,6 +1,17 @@
 import pandas as pd
+import plotly.express as px
 X_df = pd.read_csv("train.feats.csv")
 y_df = pd.read_csv("train.labels.0.csv")
 df = pd.concat((X_df, y_df), axis=1)
-x = df.loc[df['אבחנה-Location of distal metastases'] != '[]']
+# x = df.loc[df['אבחנה-Location of distal metastases'] != '[]']
 df.drop(columns=['אבחנה-Surgery date3', 'אבחנה-Surgery name3', 'אבחנה-Tumor depth', 'אבחנה-Tumor width'], inplace=True)
+col = df["אבחנה-KI67 protein"]
+print(col.unique())
+# fig = px.histogram(df,
+#                  x="אבחנה-Age",
+#                  color="אבחנה-Location of distal metastases",
+#                  hover_data=df.columns,
+#                  title="Distribution of  Diseases",
+#                  )
+# fig.show()
+
